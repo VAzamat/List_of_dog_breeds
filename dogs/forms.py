86ff3749469f6e3634e182c586b0d74c4a00a545
Valeev Azamat <azamat@mail.sao.ru>
2025-03-27 13:26:36 +0300

    dog apps: StyleFormMixin for better stylization

--- dogs/forms.py
+++ dogs/forms.py
@@ -1,8 +1,18 @@
 from django import forms
+from django.forms import BooleanField
 
 from dogs.models import Dogs, Breed
 
-class DogsForm(forms.ModelForm):
+class StyleFormMixin:
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            if isinstance(field, BooleanField):
+                field.widget.attrs['class'] = 'form-check-input'
+            else:
+                field.widget.attrs['class'] = 'form-control text-start'
+
+class DogsForm(StyleFormMixin, forms.ModelForm):
     class Meta:
         model = Dogs
         fields = '__all__'
