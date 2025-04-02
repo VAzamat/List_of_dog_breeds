f66a022b3a8e8bd5230957053b57560545d796e8
Valeev Azamat <azamat@mail.sao.ru>
2025-04-02 21:03:36 +0300

    users app: use PhoneNumberField package in **User** model

--- users/models.py
+++ users/models.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import AbstractUser
+from phonenumber_field.modelfields import PhoneNumberField
 from django.db import models
 
 NULLABLE = {'null':True, 'blank': True}
@@ -6,10 +7,17 @@ NULLABLE = {'null':True, 'blank': True}
 class User(AbstractUser):
     username = None
 
-    phone = models.CharField(max_length=35, verbose_name='телефон', help_text='Введите телефон пользователя', **NULLABLE)
+    phone = PhoneNumberField( verbose_name='телефон', help_text='Введите телефон пользователя', **NULLABLE)
     telegram_nickname = models.CharField(max_length=60, verbose_name='Ник в Telegram', help_text='Введите никнейм в Telegram', **NULLABLE)
     avatar = models.ImageField(upload_to='users/avatars/', verbose_name='Фотография пользователя', help_text='Выберете и загрузите фотографию для профиля пользователя')
     email = models.EmailField(unique=True, verbose_name='почта', help_text='Почта пользователя как уникальное поле регистрации')
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+
+    def __str__(self):
+        return self.email
